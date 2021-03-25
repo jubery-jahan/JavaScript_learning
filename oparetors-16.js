3e8e35241a681db54be6b2d16caa887ead81a912
Jubery <76787670+jubery-jahan@users.noreply.github.com>
2021-03-25 18:16:59 +0600

    Add files via upload

---
+++ oparetors-16.js
@@ -0,0 +1,30 @@
+var a=10
+var b=20
+
+console.log(a+b)
+a+=b
+
+console.log(a)
+b-=a
+console.log(b)
+var a=9
+var b=66
+console.log(a%b)
+ 
+console.log(++a)
+console.log(a++)
+
+console.log(a==b)
+console.log(a!=b)
+console.log(a>b)
+console.log(a<b)
+ console.log(a<=b)
+ console.log(a>=b)
+  
+ var c="50"
+ var d= 50
+
+ console.log(c==d)
+ console.log(c===d)
+ console.log(c!==d)
+
