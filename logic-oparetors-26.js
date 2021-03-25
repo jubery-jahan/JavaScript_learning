9a6c257abf4c44933275407191c5e6378471e85d
Jubery <76787670+jubery-jahan@users.noreply.github.com>
2021-03-25 19:53:53 +0600

    Add files via upload

---
+++ logic-oparetors-26.js
@@ -0,0 +1,27 @@
+var a=30
+var b=20
+var c=30
+var d=40
+
+if (a>b && c>d){
+    console.log("a is greater than b and cis greater than d")
+}
+
+else{
+    console.log("at least on condition is false")
+
+}
+if (a>b || c>d){
+    console.log("a is greater than b and cis greater than d")
+}
+
+else{
+    console.log("at least on condition is false")
+
+}
+
+var a=30
+var b=35
+var check=!(a>b)
+console.log(check)
+
---
+++ ternary-oparetor-27.js
@@ -0,0 +1,4 @@
+var n=10
+var str =""
+ var result=n%2===0 ? 'EVEN':'ODD'
+ console.log(result)
