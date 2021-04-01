071be7936a07a12029686db7ae012f9848abdfa1
Jubery <76787670+jubery-jahan@users.noreply.github.com>
2021-04-01 22:49:23 +0600

    Add files via upload

---
+++ nested-loop33.js
@@ -0,0 +1,21 @@
+var n=10
+for(i=1;i<=10;i++)
+{
+    var result=""
+    for(j=1;j<=i;j++)
+    {
+     result +=j+""
+    }
+    console.log(result)
+}
+
+var n=5
+for(i=1;i<=10;i++)
+{
+    var result=""
+    for(j=1;j<=n;j++)
+    {
+     result +="*"
+    }
+    console.log(result)
+}
